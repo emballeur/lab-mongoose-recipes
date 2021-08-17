19f9645b5e6a3d0adf25b3dccb32aae7ee78beff
sina tams <sina.tams@web.de>
2021-08-17 14:59:02 +0200

    my version lost partner on the way

--- index.js
+++ index.js
@@ -1,27 +1,52 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+
+/*const RecipeModel = require("./models/Recipe.model");*/
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
-// Import of the data from './data.json'
-const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const Recipe = require("./models/Recipe.model");
+// Import of the data from './data.json'
+
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
-  .then(self => {
+  /*.then((response) => {
+  console.log("mongo connection established");
+  return mongoose.connection.dropDatabase();
+})*/
+
+  .then((self) => {
     console.log(`Connected to the database: "${self.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    // Before adding any recipes to the database, let's remove all existing ones*/
+    return Recipe.deleteMany();
   })
   .then(() => {
+    /*return Recipe.create({ title: "Recipe2",  });*/
     // Run your code here, after you have insured that the connection was made
+    const data = require("./data");
+    return Recipe.insertMany(data);
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+
+  // iteration 4
+  .then(() => {
+    Recipe.findOneAndUpdate(
+      { title: "Rigatoni alla Genovese" },
+      { duration: 100 },
+      { new: true }
+    );
+  })
+
+  //iteration 5
+  .then(() => {
+    return Recipe.deleteOne({ title: "Carrot Cake" });
+  })
+
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });
